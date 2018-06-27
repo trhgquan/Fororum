de043a16c8f828de56b61d5fbfbfb8e8bb5a7a96
trhgquan <mrthedzam@gmail.com>
2018-06-27 18:32:58 +0700

    added migrations and some css.

---
+++ database/migrations/2018_06_27_113143_create_forum_categories_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateForumCategoriesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('forum_categories', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->string('keyword', 40);
+			$table->string('title', 40);
+			$table->text('description', 65535);
+			$table->timestamps();
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('forum_categories');
+	}
+
+}
